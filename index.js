754fb75e8121f7013d18269dbe0a4fb7d0bf1a13
Kendra Allen <kallen5@ufl.edu>
2025-06-01 21:12:12 -0400

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+var questionsArr = [
+    {
+        question: 'The Unicorn is the national animal of Scotland',
+        answer: true
+    },
+    {
+        question: 'Three strikes in a row in bowling is called an egale',
+        answer: false
+    },
+    {
+        question: 'Apples and pears are part of the rose famly',
+        answer: true
+    },
+    {
+        question: 'Greenland is three times the size of the state of Texas',
+        answer: true
+    },
+    {
+        question: 'The liver is the second heaviest organ in the body',
+        answer: true
+    }
+
+]
+
+function runQuiz {
+    for (var i = 0; i < 5; i++)
+
+}
+
