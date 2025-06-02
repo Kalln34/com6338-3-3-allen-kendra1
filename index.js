9025e1cbf9b3d4aa4624d76b3adfa4eab8414e57
Kendra Allen <kallen5@ufl.edu>
2025-06-01 23:37:50 -0400

    modified index.js

--- index.js
+++ index.js
@@ -2,36 +2,39 @@
 function runQuiz() {
     var questionsArr = [
         {
-            question: 'The Unicorn is the national animal of Scotland',
+            question: "The Unicorn is the national animal of Scotland.",
             answer: true
         },
         {
-            question: 'Three strikes in a row in bowling is called an egale',
+            question: "Three strikes in a row in bowling is called an egale.",
             answer: false
         },
         {
-            question: 'Apples and pears are part of the rose famly',
+            question: "Apples and pears are part of the rose famly.",
             answer: true
         },
         {
-            question: 'Greenland is three times the size of the state of Texas',
+            question: "Greenland is three times the size of the state of Texas.",
             answer: true
         },
         {
-            question: 'The liver is the second heaviest organ in the body',
+            question:"The liver is the second heaviest organ in the body.",
             answer: true
-        }
+        },
 
     ];
+
+
     var numOfTrue = 0
     for (var i = 0; i < questionsArr.length; i++) {
         var question = questionsArr[i]
+        console.log(question)
         var answer = confirm(question)
         if (answer) {
             numOfTrue++
         }
     }
-    if (numOfTrue >= Math.round(questionsArr.length)) {
+    if (numOfTrue >= 4) {
         alert("You are smart")
     } else {
         alert("Now you know")
